9faa9ee42f04bf3e4615a44289c0378470a27c45
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-15 15:39:41 -0600

    first react code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloCullen from './HelloCullen';
 
+// App is a component
 function App() {
+  let myName = 'josh';
+
+  let myH1 = <h1>Go home, {myName}, you're Drunk</h1>;
+  // 'HTML' is acutally 'JSX'
+  // "Javascript eXtended"
+  // App.jsk
   return (
     <div className="App">
+      {myH1}
       <header className="App-header">
+        {/*HelloCullen is a react component */}
+        <HelloCullen />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/HelloCullen.jsx
@@ -0,0 +1,14 @@
+function HelloCullen() {
+  return (
+    <>
+      <h2>Hello Cullen!!!</h2>
+      <h3>How are You??</h3>
+    </>
+  );
+}
+
+// export in node
+// module.exports = HelloCullen;
+
+// export in client
+export default HelloCullen;
